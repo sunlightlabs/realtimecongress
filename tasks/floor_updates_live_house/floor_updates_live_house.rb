21f57a4a1b2bc469f56930daa49058da09e997f2
Eric Mill <konklone@gmail.com>
2013-01-08 12:03:53 -0500

    Updated house floor link detector

--- tasks/floor_updates_live_house/floor_updates_live_house.rb
+++ tasks/floor_updates_live_house/floor_updates_live_house.rb
@@ -80,7 +80,7 @@ class FloorUpdatesLiveHouse
       
       doc = Nokogiri::HTML html
       
-      elem = doc.css("a.downloadLink").first
+      elem = doc.css("a[title='Download Floor Proceedings in XML Format']").first
       unless elem
         Report.warning self, "Couldn't find download link, can't go on"
         return nil
