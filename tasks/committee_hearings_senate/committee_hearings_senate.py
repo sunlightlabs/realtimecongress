2cc553f9b0b202fec2ccc82c44d69c7644210979
Eric Mill <eric@konklone.com>
2013-10-26 17:57:30 -0400

    backporting fix for jcc committees

--- tasks/committee_hearings_senate/committee_hearings_senate.py
+++ tasks/committee_hearings_senate/committee_hearings_senate.py
@@ -11,39 +11,45 @@ def run(db, es, options = {}):
       page = urllib2.urlopen("http://www.senate.gov/general/committee_schedules/hearings.xml")
     except:
       db.note("Couldn't load Senate hearings feed, can't proceed")
-      
+
     else:
       soup = BeautifulStoneSoup(page)
       meetings = soup.findAll('meeting')
       parser = HTMLParser.HTMLParser()
-      
+
       count = 0
-      
+
       for meeting in meetings:
           if re.search("^No.*?scheduled\.?$", meeting.matter.contents[0]):
             continue
-            
+
           full_id = meeting.cmte_code.contents[0].strip()
-          committee_id, subcommittee_id = re.search("^([A-Z]+)(\d+)$", full_id).groups()
-          if subcommittee_id == "00": 
+
+          match = re.search("^([A-Z]+)(\d+)$", full_id)
+          if match:
+            committee_id, subcommittee_id = match.groups()
+          else:
+            committee_id, subcommittee_id = full_id, None
+
+          if (subcommittee_id == "00") or (subcommittee_id == None):
             subcommittee_id = None
           else:
             subcommittee_id = full_id
-          
+
           committee = committee_for(db, committee_id)
-          
+
 
           # Don't warn if it's a bill-specific conference committee
           if not committee and committee_id != "JCC":
             db.warning("Couldn't locate committee by committee_id %s" % committee_id, {'committee_id': committee_id})
-          
+
           committee_url = meeting.committee['url']
 
           date_string = meeting.date.contents[0].strip()
           occurs_at = datetime.datetime(*time.strptime(date_string, "%d-%b-%Y %I:%M %p")[0:6], tzinfo=rtc_utils.EST())
           legislative_day = occurs_at.strftime("%Y-%m-%d")
           session = rtc_utils.current_session(occurs_at.year)
-          
+
           try:
             time_str = meeting.time.contents[0].strip()
             time_of_day = time.strptime(time_str, "%I:%M %p")
@@ -51,11 +57,11 @@ def run(db, es, options = {}):
             time_of_day = time_of_day.strftime("%I:%M%p")
           except ValueError:
             time_of_day = None
-          
+
           document = None
           if meeting.document:
             document = meeting.document.contents[0].strip()
-              
+
           room = meeting.room.contents[0].strip()
           description = meeting.matter.contents[0].strip().replace('\n', '')
 
@@ -63,12 +69,12 @@ def run(db, es, options = {}):
           description = parser.unescape(parser.unescape(description))
 
           bill_ids = rtc_utils.extract_bills(description, session)
-          
+
 
           documents = db['committee_hearings'].find({
-            'chamber': 'senate', 
-            'committee_id': committee_id, 
-              
+            'chamber': 'senate',
+            'committee_id': committee_id,
+
             "$or": [{
               'occurs_at': occurs_at
               },{
@@ -81,35 +87,35 @@ def run(db, es, options = {}):
             hearing = documents[0]
           else:
             hearing = {
-              'chamber': 'senate', 
+              'chamber': 'senate',
               'committee_id': committee_id
             }
 
             hearing['created_at'] = datetime.datetime.now()
-          
+
           if subcommittee_id:
             hearing['subcommittee_id'] = subcommittee_id
           hearing['updated_at'] = datetime.datetime.now()
-          
+
           hearing.update({
             'bill_ids': bill_ids,
             'occurs_at': occurs_at,
-            'room': room, 
-            'description': description, 
+            'room': room,
+            'description': description,
             'legislative_day': legislative_day,
             'time_of_day': time_of_day,
             'session': session,
             'committee_url': committee_url,
             'dc': True
           })
-          
+
           if committee:
             hearing['committee'] = committee
-          
+
           db['committee_hearings'].save(hearing)
-          
+
           count += 1
-      
+
       db.success("Updated or created %s Senate committee hearings" % count)
 
 def committee_for(db, committee_id):
