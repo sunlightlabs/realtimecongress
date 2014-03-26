37e29ee60b6322700a511bcce64032d1a28056bb
Eric Mill <eric@konklone.com>
2014-03-26 11:16:06 -0400

    More unused models

--- models/clip.rb
+++
@@ -1,8 +0,0 @@
-class Clip
-
-  include ::Searchable::Model
-  
-  result_fields :video_id, :id, :video_clip_id, :offset, :duration, :events, :srt_link, :legislator_names, :rolls, :bills, :bioguide_ids
-  searchable_fields :events, :captions, :legislator_names
-
-end
